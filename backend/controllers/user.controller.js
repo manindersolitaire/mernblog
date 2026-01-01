1b60dc2015c0d72197f920c5dc83139f2612e476
maninderdev <maninderdev8@gmail.com>
2026-01-01 12:24:08 +0530

    minor project day 3

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const register = async(req,res)=>{
     try {
@@ -52,3 +53,57 @@ export const register = async(req,res)=>{
         })
     }
 }
+
+export const login = async(req,res)=>{
+    try {
+        const {email , password} =  req.body
+        if(!email || !password){
+            return res.status(400).json({
+                success : false,
+                message : 'All fields are required'
+            })
+        }
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success : false,
+                message : 'Incorrect email or password'
+            })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success : false,
+                message : 'Invalid Credentials'
+            })
+        }
+
+        const token =  await jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn : '1d'})
+
+        return res.status(200).cookie("token", token, {maxAge : 1*24*60*60*1000, httpsOnly : true , sameSite: "strict"}).json({
+            success : true,
+            message : `Welcome back ${user.firstName}, ${token}`,
+            user
+        })
+
+    } catch (error) {
+        console.log('Error in login controller', error)
+        return res.status(500).json({
+            success : false,
+            message : "failed to login"
+        })
+    }
+}
+
+export const logout = async(req,res)=>{
+    try {
+        return res.status(200).cookie("token", "", {maxAge : 0}).json({
+            message : "Logged out successfully",
+            success : true
+        })        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
